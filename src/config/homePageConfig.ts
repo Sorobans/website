cc1f50ac9dfb8780cea0ddb57749cb805332effa
Austin-Pynes <austinpyne14@gmail.com>
2026-03-20 18:27:45 +1300

    Update homePageConfig.ts

--- src/config/homePageConfig.ts
+++ src/config/homePageConfig.ts
@@ -1,11 +1,11 @@
 // Home / BlogIndex Config
 export const homePageProfile = {
-  name: 'Snow',
-  tagline: '“爬起仅仅只是因为不想输。”',
+  name: 'Soroban',
+  tagline: '“ I prefer the depth of the ocean to the noise of the waves. ”',
   avatarUrl: '/img/724d7fb480c8ac0db472ca5c7e36d239.jpg',
   avatarAlt: 'SnowAvatar',
   backgroundUrl: '/img/site_header_1920.webp',
-  footerText: '© Snowの小窝 - 2022-2025',
+  footerText: '© Soroban 2026',
 };
 
 export const homePageLinks = {
